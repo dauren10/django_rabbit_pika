0a8af820f5f04f77998827e7c196186f58c805be
OEM Configuration (temporary user) <d.shalabayev@kazdream.kz>
2023-08-16 18:34:39 +0600

    pika

--- test/views.py
+++ test/views.py
@@ -1,10 +1,10 @@
-from rest_framework import viewsets
-from rest_framework.response import Response
 from rest_framework.views import APIView
-from .tasks import add
-from time import sleep
+import pika
+import uuid
+from rest_framework.response import Response
 class TestListView(APIView):
-    def get(self,format=None):
+    def get(self, request, format=None):
         
-        result = add.delay(4,4)
+
         return Response('Hello')
+

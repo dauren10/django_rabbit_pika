064207ff2d3c8b274fda6321802838acf1923fb5
Dauren Shalabayev <dauren.shalabayev@gmail.com>
2023-08-18 21:12:26 +0600

    2 endpoint

--- test/urls.py
+++ test/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TestListView
+from .views import TestListView,SampleListView
 
 urlpatterns = [
-    path('test', TestListView.as_view(),name='test'),
+    path('test', TestListView.as_view(), name='test'),
+    path('sample', SampleListView.as_view(), name='sample'),
 ]
--- test/views.py
+++ test/views.py
@@ -4,48 +4,51 @@ from rest_framework.response import Response
 
 class TestListView(APIView):
     # 1 case
-    # def get(self, request, format=None):
-    #     try:
-    #         # RabbitMQ connection parameters
-    #         rabbitmq_host = '172.17.0.1'  # Replace with your RabbitMQ server's hostname or IP address
-    #         rabbitmq_port = 5672  # Default port
-    #         rabbitmq_virtual_host = '/'  # Default virtual host
-    #         rabbitmq_credentials = pika.PlainCredentials('guest', 'guest')  # Replace with your credentials if required
+    def get(self, request, format=None):
+        try:
+            # RabbitMQ connection parameters
+            rabbitmq_host = '172.17.0.1'  # Replace with your RabbitMQ server's hostname or IP address
+            rabbitmq_port = 5672  # Default port
+            rabbitmq_virtual_host = '/'  # Default virtual host
+            rabbitmq_credentials = pika.PlainCredentials('guest', 'guest')  # Replace with your credentials if required
 
-    #         # Establish connection with RabbitMQ
-    #         connection_params = pika.ConnectionParameters(
-    #             host=rabbitmq_host,
-    #             port=rabbitmq_port,
-    #             virtual_host=rabbitmq_virtual_host,
-    #             credentials=rabbitmq_credentials
-    #         )
-    #         connection = pika.BlockingConnection(connection_params)
-    #         channel = connection.channel()
+            # Establish connection with RabbitMQ
+            connection_params = pika.ConnectionParameters(
+                host=rabbitmq_host,
+                port=rabbitmq_port,
+                virtual_host=rabbitmq_virtual_host,
+                credentials=rabbitmq_credentials
+            )
+            connection = pika.BlockingConnection(connection_params)
+            channel = connection.channel()
 
-    #         # Declare queue 'messages' if it doesn't exist
-    #         channel.queue_declare(queue='messages')
+            # Declare queue 'messages' if it doesn't exist
+            channel.queue_declare(queue='messages')
             
-    #         # Add a binding to the queue (example binding to an exchange named 'logs')
-    #         exchange_name = 'logs'
-    #         channel.exchange_declare(exchange=exchange_name, exchange_type='fanout')
-    #         channel.queue_bind(exchange=exchange_name, queue='messages')
+            # Add a binding to the queue (example binding to an exchange named 'logs')
+            exchange_name = 'logs'
+            channel.exchange_declare(exchange=exchange_name, exchange_type='fanout')
+            channel.queue_bind(exchange=exchange_name, queue='messages')
 
-    #         # Send a message
-    #         message = 'Hello, this is a test message!'
-    #         channel.basic_publish(exchange='', routing_key='messages', body=message)
+            # Send a message
+            message = 'Hello, this is a test message!'
+            channel.basic_publish(exchange='', routing_key='messages', body=message)
 
-    #         # Close the connection
-    #         connection.close()
+            # Close the connection
+            connection.close()
 
-    #         return Response('Message sent successfully.')
+            return Response('Message sent successfully.')
 
-    #     except pika.exceptions.AMQPConnectionError as e:
-    #         return Response(f'Error connecting to RabbitMQ: {e}')
+        except pika.exceptions.AMQPConnectionError as e:
+            return Response(f'Error connecting to RabbitMQ: {e}')
 
-    #     except Exception as e:
-    #         return Response(f'An error occurred: {e}')
+        except Exception as e:
+            return Response(f'An error occurred: {e}')
         
 
+    
+
+class SampleListView(APIView):
     #2 case
     def get(self, request, format=None):
         try:
